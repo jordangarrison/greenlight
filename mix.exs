5092300fe7acf25aa5ff7347911466af54d1c1b7
Jordan Garrison <jordangarrison@users.noreply.github.com>
2026-03-03 23:08:17 -0600

    ci: add release-please version annotations to mix.exs and package.nix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Greenlight.MixProject do
   def project do
     [
       app: :greenlight,
-      version: "0.1.0",
+      version: "0.1.0", # x-release-please-version
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
--- nix/package.nix
+++ nix/package.nix
@@ -10,7 +10,7 @@
 
 let
   pname = "greenlight";
-  version = "0.1.0";
+  version = "0.1.0"; # x-release-please-version
   src = ./..;
 
   mixFodDeps = fetchMixDeps {
